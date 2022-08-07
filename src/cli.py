8b00889130db6534d45f0ae7013c8a99f09d5d17
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-08-07 23:12:31 +0900

    feat: Create application class

--- src/cli.py
+++ src/cli.py
@@ -1,33 +1,9 @@
-import argparse
-import subprocess
+from typing import Optional
+from typing import Sequence
 
-parser = argparse.ArgumentParser(description="checkstyle launcher")
-parser.add_argument(
-    "-c",
-    "--config",
-    type=str,
-    default="/google_checks.xml",
-    help="checkstyle configuration file",
-)
-parser.add_argument(
-    "path",
-    type=str,
-)
+from . import application
 
 
-def main():
-    args = parser.parse_args()
-    run_checkstyle(
-        config=args.config,
-        path=args.path,
-    )
-
-
-def run_checkstyle(config: str, path: str):
-    cmd = ['java', '-jar']
-    args = ['../checkstyle-10.3.2-all.jar', '-c', config, path]
-    subprocess.run(cmd+args)
-
-
-if __name__ == "__main__":
-    main()
+def main(argv: Optional[Sequence[str]] = None):
+    app = application.Application()
+    app.run(argv)
