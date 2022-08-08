05b8ace69cff772064d9a769f4ebc61544a6a39e
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-08-08 16:04:04 +0900

    feat: Create main package

--- src/main/application.py
+++ src/main/application.py
@@ -5,7 +5,7 @@ from typing import Optional
 from typing import Sequence
 from typing import Tuple
 
-from . import utils
+from main import utils
 
 
 class Application:
--- src/main/cli.py
+++ src/main/cli.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from typing import Sequence
 
-from . import application
+from main import application
 
 
 def main(argv: Optional[Sequence[str]] = None):
