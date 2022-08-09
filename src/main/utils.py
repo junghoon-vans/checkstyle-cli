a8d5739df53347b16bc055c819841f53412bef08
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-08-09 19:10:02 +0900

    feat: Add version argv to arg_parser

--- src/main/utils.py
+++ src/main/utils.py
@@ -11,6 +11,13 @@ def arg_parser() -> argparse.ArgumentParser:
         help="checkstyle configuration file",
     )
     parser.add_argument(
+        "-v",
+        "--version",
+        type=str,
+        default="10.3.2",
+        help="checkstyle version",
+    )
+    parser.add_argument(
         "files", nargs="*",
         help="files to verify",
     )
