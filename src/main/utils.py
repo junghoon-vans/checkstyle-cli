fc42d49ff515a00062638ba2dfda72b25fe93512
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-08-09 21:04:35 +0900

    feat: Add download_checkstyle function

--- src/main/utils.py
+++ src/main/utils.py
@@ -1,4 +1,7 @@
 import argparse
+import os
+
+import requests
 
 
 def arg_parser() -> argparse.ArgumentParser:
@@ -22,3 +25,34 @@ def arg_parser() -> argparse.ArgumentParser:
         help="files to verify",
     )
     return parser
+
+
+def download_checkstyle(version: str) -> None:
+    try:
+        download(
+            url=get_checkstyle_download_url(version),
+            filename=get_checkstyle_filename(version),
+        )
+    except requests.exceptions.HTTPError as e:
+        print(e)
+
+
+def get_checkstyle_download_url(version: str) -> str:
+    _prefix_url = "https://github.com/checkstyle/checkstyle/" \
+        "releases/download/checkstyle-{version}/"
+    download_url = _prefix_url.format(version=version)
+    return download_url
+
+
+def get_checkstyle_filename(version: str) -> str:
+    _prefix_filename = "checkstyle-{version}-all.jar"
+    filename = _prefix_filename.format(version=version)
+    return filename
+
+
+def download(url: str, filename: str) -> None:
+    r = requests.get(url + filename)
+    r.raise_for_status()
+
+    with open(os.path.join(os.getcwd(), filename), "wb") as f:
+        f.write(r.content)
