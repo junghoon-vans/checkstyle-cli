cebffae8da71866616d9f9c9c2d2559832c2b70b
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-08-15 14:29:55 +0900

    refactor: Add store to utils package

--- src/checkstyle/application.py
+++ src/checkstyle/application.py
@@ -1,24 +1,25 @@
 from typing import Optional
 from typing import Sequence
 
-from checkstyle.utils import download_checkstyle
-from checkstyle.utils import get_checkstyle_cache
 from checkstyle.utils import run_command
 from checkstyle.utils.parser import Parser
+from checkstyle.utils.store import Store
 
 
 class Application:
     def __init__(self) -> None:
         self._parser = Parser()
+        self._store = Store()
 
     def run(self, argv: Optional[Sequence[str]]) -> int:
         args_dict = self._parser.parse_args_dict(argv)
 
-        binary_file = download_checkstyle(args_dict.pop('version'))
+        checkstyle_version = args_dict.pop('version')
+        binary_file = self._store.download_checkstyle(checkstyle_version)
         files = args_dict.pop('files')
 
         exit_code = run_command(
-            target=get_checkstyle_cache(binary_file),
+            target=self._store.get_checkstyle_cache(binary_file),
             args=self._parser.convert_args_dict_to_list(args_dict) + files,
         )
         return exit_code
--- src/checkstyle/utils/__init__.py
+++ src/checkstyle/utils/__init__.py
@@ -1,9 +1,5 @@
-import os
 import subprocess
 from typing import List
-
-import requests
-from tqdm import tqdm
 
 
 def run_command(target: str, args: List[str]) -> int:
@@ -27,56 +23,3 @@ def run_command(target: str, args: List[str]) -> int:
         )
 
     return exit_code
-
-
-def download_checkstyle(version: str) -> str:
-    filename = get_checkstyle_filename(version)
-    if not is_exist_file(filename):
-        try:
-            url = get_checkstyle_download_url(version)
-            download(url=url, filename=filename)
-        except requests.exceptions.HTTPError as e:
-            print(e)
-    return filename
-
-
-def is_exist_file(filename: str) -> bool:
-    return os.path.exists(get_checkstyle_cache(filename))
-
-
-def get_checkstyle_download_url(version: str) -> str:
-    _prefix_url = "https://github.com/checkstyle/checkstyle/" \
-        "releases/download/checkstyle-{version}/"
-    download_url = _prefix_url.format(version=version)
-    return download_url
-
-
-def get_checkstyle_filename(version: str) -> str:
-    _prefix_filename = "checkstyle-{version}-all.jar"
-    filename = _prefix_filename.format(version=version)
-    return filename
-
-
-def get_checkstyle_cache(filename: str) -> str:
-    cache_path = os.path.join(os.getcwd(), ".checkstyle_cache")
-    if not os.path.exists(cache_path):
-        os.makedirs(cache_path)
-    return os.path.join(cache_path, filename)
-
-
-def download(url: str, filename: str) -> None:
-    r = requests.get(url + filename, stream=True)
-    r.raise_for_status()
-    total = int(r.headers.get('Content-Length', 0))
-
-    with open(get_checkstyle_cache(filename), "wb") as f, tqdm(
-        desc=filename,
-        total=total,
-        unit='iB',
-        unit_scale=True,
-        unit_divisor=1024,
-    ) as bar:
-        f.write(r.content)
-        for data in r.iter_content(chunk_size=1024):
-            size = f.write(data)
-            bar.update(size)
