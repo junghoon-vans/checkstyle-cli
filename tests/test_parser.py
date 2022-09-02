ed9ad781720a7dfa26625c228675e14dd267259f
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-09-03 02:09:30 +0900

    test: Update test_parser.py

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,8 @@
 import pytest
 from checkstyle.utils.parser import Parser
 
+from src.checkstyle import default_runtime
+
 
 @pytest.fixture
 def parser() -> Parser:
@@ -10,5 +12,6 @@ def parser() -> Parser:
 
 def test_parse_args(parser: Parser):
     assert parser.parse_args_dict(None) == {
-        'config': '/google_checks.xml', 'version': 'latest', 'files': [],
+        'config': '/google_checks.xml',
+        'version': default_runtime, 'files': [],
     }
