1f49ab3d8f76ac4779814f79ed68d3ea4fb91335
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-09-14 15:08:23 +0900

    chore: Add sphinx config files

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'checkstyle-cli'
+copyright = '2022, junghoon-vans'
+author = 'junghoon-vans'
+
+# -- General configuration ---------------------------------------------------
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
