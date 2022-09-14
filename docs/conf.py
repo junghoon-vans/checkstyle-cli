b6122950d6c4b2a08133f71bd4447f604e7b8602
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-09-14 15:18:58 +0900

    chore: Set theme of sphinx to furo

--- docs/conf.py
+++ docs/conf.py
@@ -11,5 +11,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'alabaster'
+html_theme = 'furo'
 html_static_path = ['_static']
