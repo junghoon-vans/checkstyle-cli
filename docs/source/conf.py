2c0ea3ca7f80d3866aa344c97c916a8dd1981c94
Jeonghun-Ban <junghoon.ban@gmail.com>
2022-09-16 22:13:29 +0900

    chore: Update sphinx configs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../src/checkstyle'))
+sys.path.insert(0, os.path.abspath('../../src/'))
 
 # -- Project information -----------------------------------------------------
 project = 'checkstyle-cli'
@@ -8,7 +8,17 @@ copyright = '2022, junghoon-vans'
 author = 'junghoon-vans'
 
 # -- General configuration ---------------------------------------------------
-extensions = ['sphinx.ext.autodoc']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+]
+
+source_suffix = '.rst'
+master_doc = 'index'
+pygments_style = 'sphinx'
 
 templates_path = ['_templates']
 exclude_patterns = ['Thumbs.db', '.DS_Store']
@@ -18,3 +28,17 @@ language = 'en'
 # -- Options for HTML output -------------------------------------------------
 html_theme = 'furo'
 html_static_path = ['_static']
+
+# -- Autodoc configuration ---------------------------------------------------
+autodoc_member_order = 'bysource'
+autoclass_content = "both"
+autosummary_generate = True
+
+autodoc_default_options = {
+    'members': True,
+    'show-inheritance': True,
+    'inherited-members': True,
+    'no-special-members': True,
+}
+
+napoleon_google_docstring = True
